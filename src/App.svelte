49882316597d44b3bfe18699ea4cd09b043c15d4
cspraggett <cspraggett@gmail.com>
2021-03-09 14:00:21 -0500

    Implement basic game play

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,72 @@
 <script>
+	import { quote } from './quotes';
 	import Tailwind from './Components/Tailwind.svelte';
 
-	export let name;
+	let q = quote();
+	let input = '';
+	let timer = 0;
+	$: start = false;
+
+	function changeQuote(i) {
+		if (q.quote === i) {
+			q = quote();
+			input = '';
+			timer = 0;
+		}
+	}
+
+	$: changeQuote(input);
+
+	$: toggleStart = () => (start = !start);
+
+	let startGame = () => {
+		toggleStart();
+		let ticker = setInterval(() => {
+			if (start) {
+				timer++;
+			} else {
+				clearInterval(ticker);
+			}
+		}, 1000);
+	};
 </script>
 
 <Tailwind />
-<main>
-	<h1 class="bg-indigo-500">Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
+<main
+	class="h-screen flex flex-col items-stretch p-2 bg-indigo-400 overflow-auto"
+>
+	<div
+		class="text-gray-800 bg-gray-200 font-bold flex justify-center items-center m-2 header"
+	>
+		<div>Time: {timer}</div>
+	</div>
+	<div
+		class=" h-auto w-full m-auto flex flex-col justify-between items-center text-center border-2 border-gray-800"
+	>
+		<div class="m-5">
+			<div class="text-center">
+				{#each q.quote as letter, index (index)}
+					<span
+						class:bg-green-500={input[index] === letter}
+						class:bg-red-500={input[index] && input[index] !== letter}
+						>{letter}</span
+					>
+				{/each}
+			</div>
+		</div>
+		<div class="m-5 w-2/3 h-1/4">
+			<textarea
+				bind:value={input}
+				type="text"
+				class="h-full w-full text-xs border border-gray-800"
+			/>
+		</div>
+	</div>
+	<div class="text-red-500 text-center italic text-sm">{q.author}</div>
+
+	<div class="h-1/5 flex justify-center space-x-2 items-center mb-2">
+		<button on:click={() => startGame()} class="bg-red-500"
+			>{!start ? 'Start' : 'Pause'}</button
+		>
+	</div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+export const shuffle = (array) => {
+	const arr = [...array];
+	for (let i = arr.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+
+		let temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	return arr;
+};
